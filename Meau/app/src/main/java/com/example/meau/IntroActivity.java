8df3556c73aefff4364063d09a3b34d534d9a083
AndreSouto <andre_luisferreira@hotmail.com>
2019-04-09 22:11:30 -0400

    First two Activities

---
+++ Meau/app/src/main/java/com/example/meau/IntroActivity.java
@@ -0,0 +1,67 @@
+package com.example.meau;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class IntroActivity extends AppCompatActivity {
+
+    Button menu,adotar,ajudar,cadastrar,login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        // Buttons
+        menu = findViewById(R.id.menu);
+        adotar = findViewById(R.id.adotar);
+        ajudar = findViewById(R.id.ajudar);
+        cadastrar = findViewById(R.id.cadastrar);
+        login = findViewById(R.id.login);
+
+//        menu.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(calc.this,Settings.class));
+//                finish();
+//            }
+//        });
+//
+//        adotar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(calc.this,Settings.class));
+//                finish();
+//            }
+//        });
+//
+//        ajudar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(calc.this,Settings.class));
+//                finish();
+//            }
+//        });
+//
+//        cadastrar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(calc.this,Settings.class));
+//                finish();
+//            }
+//        });
+//
+//        login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(calc.this,Settings.class));
+//                finish();
+//            }
+//        });
+
+
+    }
+}
--- Meau/app/src/main/java/com/example/meau/MainActivity.java
+++ Meau/app/src/main/java/com/example/meau/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.meau;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        main = findViewById(R.id.main);
+    }
+
+    /** Changing activity when touching anywhere on the screen */
+    public void introScreen(View view) {
+        Intent intent = new Intent(this, IntroActivity.class);
+        startActivity(intent);
     }
 }
